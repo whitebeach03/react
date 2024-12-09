efc1d940d978524ff37e240e8e7978ebf73d33fa
whitebeach03 <whitehama3@icloud.com>
2024-12-10 04:00:56 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,5 @@
-// import logo from './logo.svg';
-import { Prev } from 'react-bootstrap/esm/PageItem';
-import './App.css';
-import React, { useState } from 'react';
 
-function App() {
-
-  const [todo, setTodo] = useState('');
-  const [todolist, setTodolist] = useState([]);
-
-
-  const handleTodo = (e) => {
-    setTodo(e.target.value);
-  };
-
-  function handleButtonClick() {
-      alert('入力されたToDo: ' + todo)
-      setTodolist((prev) => [...prev, todo])
-  }
-
-  return (
-    <div className="App">
-      <h1>Hello World.</h1>
-      <h2>My Name Is Takumi Shirahama.</h2>
-
-      <input 
-        type='text'  
-        placeholder='ToDoの入力' 
-        onChange={handleTodo}
-      />
-
-      <button onClick={handleButtonClick}> ケツ穴確定ボタン </button>
-
-      {
-        todolist.map(
-           (todo, index) => {
-            return (
-              <p key={index}>{todo}</p>
-            );
-           }
-        )
-      }
-
-    </div>
-  );
+export default function Square() {
+  return <button className='square'>X</button>;
 }
 
-export default App;
