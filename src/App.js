25a12946b802b3090ee0f78d45851905802b0f56
whitebeach03 <whitehama3@icloud.com>
2024-12-10 04:26:33 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
 
-export default function Square() {
-  return <button className='square'>X</button>;
+// export default function Square() {
+//   return <button className='square'>X</button>;
+// }
+
+// export default function Board() {
+//   return (
+//     <>
+//     <div className="board-row">
+//       <button className='square'>1</button>
+//       <button className='square'>2</button>
+//       <button className='square'>3</button>
+//     </div>
+//     <div className="board-row">
+//       <button className='square'>4</button>
+//       <button className='square'>5</button>
+//       <button className='square'>6</button>
+//     </div>
+//     <div className="board-row">
+//       <button className='square'>7</button>
+//       <button className='square'>8</button>
+//       <button className='square'>9</button>
+//     </div>
+//     </>
+//   );
+// }
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value='1'/>
+        <Square value='2'/>
+        <Square value='3'/>
+      </div>
+      <div className="board-row">
+        <Square value='4'/>
+        <Square value='5'/>
+        <Square value='6'/>
+      </div>
+      <div className="board-row">
+        <Square value='7'/>
+        <Square value='8'/>
+        <Square value='9'/>
+      </div>
+    </>
+  );
 }
 
